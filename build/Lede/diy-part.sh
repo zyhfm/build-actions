0f319500c5da90b6796f3720cbaa9635442c6d1c
zyhfm <zenyahui@163.com>
2025-07-24 23:09:13 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -3,7 +3,8 @@
 # DIY扩展二合一了，在此处可以增加插件
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
-
+src-git kenzo https://github.com/kenzok8/openwrt-packages
+src-git small https://github.com/kenzok8/small
 
 # 后台IP设置
 export Ipv4_ipaddr="192.168.2.8"            # 修改openwrt后台地址(填0为关闭)
