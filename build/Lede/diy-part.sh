185d113112c538e4d46604ea96e256283eff30b2
zyhfm <zenyahui@163.com>
2025-07-24 23:28:14 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -3,9 +3,9 @@
 # DIY扩展二合一了，在此处可以增加插件
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
-git clone https://github.com/kenzok8/small-package.git package/small-package
-git clone https://github.com/kenzok8/small.git package/openwrt-small
-git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan  #微信推送
+src-git https://github.com/kenzok8/small-package.git package/small-package
+src-git https://github.com/kenzok8/small.git package/openwrt-small
+src-git https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan  #微信推送
 
 # 后台IP设置
 export Ipv4_ipaddr="192.168.2.8"            # 修改openwrt后台地址(填0为关闭)
